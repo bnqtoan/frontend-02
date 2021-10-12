2e10caba4baed58b3b972b95890ea8da35d65395
Toan Bui (Flexer) <bnqtoan@gmail.com>
2021-10-12 10:48:56 +0700

    add tailwind full config

--- sample/tailwind.config.js
+++ sample/tailwind.config.js
@@ -5,24 +5,12 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    container: {
-        center: true
-    },
-    extend: {
-      fontSize: {
-          "h1": ["72px", "98px"]
-      },
-      colors: {
-        "gray": {
-          900: "#18191F"
-        }
-      }
-    },
+    
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    
   ],
 }
